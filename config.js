cb8b2f011f22904fe3d08b2aed484131085de13b
toonflix-hindi <fk5228280@gmail.com>
2026-09-11 21:03:08 +0530

    Update Firebase API key in config.js

--- config.js
+++ config.js
@@ -3,5 +3,5 @@
    The Realtime Database URL is already set for this project. */
 window.TOONFLIX_CONFIG = {
   FIREBASE_DB_URL: "https://toonflix-wed-default-rtdb.firebaseio.com",
-  FIREBASE_API_KEY: "PASTE_YOUR_FIREBASE_WEB_API_KEY_HERE"
+  FIREBASE_API_KEY: "BEaisX3kn1kOD1GLOEQPZcDsA0EK1b97SPxOs3rd5AcTcQKToIJEEMtd-GsjhAxu707Q-GHtfyWuxQNqLsjuma0"
 };
